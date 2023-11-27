d28c75f18cb25eae1838131b5c5437c369fd04b0
ZanderCowboy <zanderkotze99@gmail.com>
2023-11-27 22:07:44 +0200

    added onLongPress feature to main_tab.dart

--- lib/presentation/home/widgets/main_tab.dart
+++ lib/presentation/home/widgets/main_tab.dart
@@ -14,48 +14,72 @@ class VerticalTab extends StatelessWidget {
   Widget build(BuildContext context) {
     final ScrollController scrollController = ScrollController();
 
-    return Card(
-      elevation: 5,
-      shadowColor: Colors.white,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(6.0),
-        child: SizedBox(
-          width: MediaQuery.sizeOf(context).width / 6,
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(child: Text(title)),
-                  const IconButton(
-                    onPressed: null,
-                    icon: Icon(Icons.add_outlined),
-                  ),
-                ],
-              ),
-              gap_10,
-              Expanded(
-                child: ScrollConfiguration(
-                  behavior: CustomScrollBehaviour(),
-                  child: ListView(
-                    controller: scrollController,
-                    children: const [
-                      EntryCard(),
-                      gap_16,
-                      EntryCard(),
-                      gap_16,
-                      EntryCard(),
-                      gap_16,
-                      EntryCard(),
-                      gap_16,
-                      EntryCard(),
-                    ],
+    return GestureDetector(
+      onLongPress: () {
+        CustomDialog.show(
+          context: context,
+          title: const Text('delete'),
+          actions: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                OutlinedButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('Cancel'),
+                ),
+                gap_10,
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Delete'),
+                ),
+              ],
+            )
+          ],
+        );
+      },
+      child: Card(
+        elevation: 5,
+        shadowColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: SizedBox(
+            width: MediaQuery.sizeOf(context).width / 4,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(child: Text(title)),
+                    const IconButton(
+                      onPressed: null,
+                      icon: Icon(Icons.add_outlined),
+                    ),
+                  ],
+                ),
+                gap_10,
+                Expanded(
+                  child: ScrollConfiguration(
+                    behavior: CustomScrollBehaviour(),
+                    child: ListView(
+                      controller: scrollController,
+                      children: const [
+                        EntryCard(),
+                        gap_16,
+                        EntryCard(),
+                        gap_16,
+                        EntryCard(),
+                        gap_16,
+                        EntryCard(),
+                        gap_16,
+                        EntryCard(),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
