b7dac80ec00087d6a1bd96d5cdfeda5ead306974
ZanderCowboy <zanderkotze99@gmail.com>
2023-11-27 22:08:35 +0200

    add list to home_page.dart for tabs

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:multichoice/constants/spacing_constants.dart';
+import 'package:multichoice/presentation/home/widgets/custom_dialog.dart';
 import 'package:multichoice/utils/custom_scroll_behaviour.dart';
 
 part 'widgets/main_tab.dart';
 part 'widgets/entry_card.dart';
+part 'widgets/empty_tab.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final ScrollController scrollController = ScrollController();
+
+  List tabs = [
+    const VerticalTab(title: 'title', subtitle: 'subtitle'),
+    const VerticalTab(title: 'title', subtitle: 'subtitle'),
+    const VerticalTab(title: 'title', subtitle: 'subtitle'),
+    const VerticalTab(title: 'title', subtitle: 'subtitle'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -16,27 +27,38 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         leading: const Drawer(),
         backgroundColor: Colors.lightBlue,
+        actions: const <Widget>[
+          IconButton(
+            onPressed: null,
+            icon: Icon(
+              Icons.add_outlined,
+            ),
+          ),
+        ],
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(12),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
         child: SizedBox(
-          height: 300,
+          height: MediaQuery.sizeOf(context).height / 1.375,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              VerticalTab(
-                title: 'To Watch',
-                subtitle: 'things to watch',
-              ),
-              gap_12,
-              VerticalTab(
-                title: 'Watching',
-                subtitle: 'things busy watching',
-              ),
-              gap_12,
-              VerticalTab(
-                title: 'Finished',
-                subtitle: 'things we finised watching',
+              Expanded(
+                child: ScrollConfiguration(
+                  behavior: CustomScrollBehaviour(),
+                  child: ListView.builder(
+                    controller: scrollController,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: tabs.length + 1,
+                    itemBuilder: (context, index) {
+                      if (index == tabs.length) {
+                        return const EmptyTab();
+                      } else {
+                        return tabs[index];
+                      }
+                    },
+                  ),
+                ),
               ),
             ],
           ),
