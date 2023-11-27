831b3b567f6c1f8364c82465da07bf1af05327ce
ZanderCowboy <zanderkotze99@gmail.com>
2023-11-27 22:07:15 +0200

    created empty_tab to add new tab

---
+++ lib/presentation/home/widgets/empty_tab.dart
@@ -0,0 +1,46 @@
+part of '../home_page.dart';
+
+class EmptyTab extends StatelessWidget {
+  const EmptyTab({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final ScrollController scrollController = ScrollController();
+
+    return GestureDetector(
+      onTap: () {
+        CustomDialog.show(
+          context: context,
+          title: const Text('New Tab'),
+          actions: <Widget>[
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('ok'),
+            ),
+          ],
+        );
+      },
+      child: Card(
+        elevation: 5,
+        color: Colors.grey[600],
+        // shadowColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: SizedBox(
+            width: MediaQuery.sizeOf(context).width / 4,
+            child: const IconButton(
+              iconSize: 36,
+              onPressed: null,
+              icon: Icon(Icons.add_outlined),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
