0a65d85270b9d4fce90afc5415d6b295525bb6a7
ZanderCowboy <zanderkotze99@gmail.com>
2023-12-03 01:10:27 +0200

    moved App to newly created file

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:multichoice/presentation/home/home_page.dart';
+import 'package:multichoice/app.dart';
 
 void main() {
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Multichoice',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      darkTheme: ThemeData.dark(),
-      home: const HomePage(),
-    );
-  }
 }
