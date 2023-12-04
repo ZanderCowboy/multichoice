438479187ef3a03b1c1fd9a60d52750c09e627b9
ZanderCowboy <zanderkotze99@gmail.com>
2023-12-05 01:14:50 +0200

    removed entries_list and added Map to tabs_list

--- lib/data/persistance/models/tabs_list.dart
+++ lib/data/persistance/models/tabs_list.dart
@@ -4,21 +4,44 @@ class TabsList {
   TabsList();
 
   static final List<VerticalTab> tabsList = [];
+  static final Map<VerticalTab, List<EntryCard>> tabsData = {};
 
   // create
   void addTab(VerticalTab verticalTab) {
     tabsList.add(verticalTab);
+    tabsData[verticalTab] = [];
+  }
+
+  void addEntryToTab(VerticalTab verticalTab, EntryCard entryCard) {
+    tabsData[verticalTab]?.add(entryCard);
   }
 
   // read
   List<VerticalTab> readTabs() {
-    return tabsList;
+    // return tabsList;
+    return tabsData.keys.toList();
+  }
+
+  List<EntryCard>? readEntries(VerticalTab verticalTab) {
+    return tabsData[verticalTab]?.toList();
   }
 
   // update
 
   // delete
-  void deleteTab(int index) {
-    tabsList.removeAt(index);
+  void deleteTab(
+    int tabIndex,
+    VerticalTab verticalTab,
+  ) {
+    tabsList.removeAt(tabIndex);
+    tabsData.remove(verticalTab);
+  }
+
+  void deleteEntryInTab(
+    VerticalTab verticalTab,
+    int entryIndex,
+    EntryCard entryCard,
+  ) {
+    tabsData[verticalTab]?.removeAt(entryIndex);
   }
 }
