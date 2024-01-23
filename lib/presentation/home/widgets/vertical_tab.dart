46fbd29d9193d2904115890de63229fcfd926ecc
ZanderCowboy <zanderkotze99@gmail.com>
2024-01-23 14:03:03 +0200

    renamed main_tab to vertical_tab

---
+++ lib/presentation/home/widgets/vertical_tab.dart
@@ -0,0 +1,46 @@
+part of '../home_page.dart';
+
+class VerticalTab extends StatelessWidget {
+  const VerticalTab({
+    required this.tabId,
+    required this.tabTitle,
+    required this.tabSubtitle,
+    super.key,
+  });
+
+  final String tabId;
+  final String tabTitle;
+  final String tabSubtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      shadowColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: circularBorder12,
+      ),
+      child: Padding(
+        padding: allPadding6,
+        child: SizedBox(
+          width: MediaQuery.sizeOf(context).width / 4,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(child: Text(tabTitle)),
+                  const IconButton(
+                    onPressed: null,
+                    icon: Icon(Icons.minor_crash_rounded),
+                  ),
+                ],
+              ),
+              gap10,
+              Cards(tabId: tabId),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
