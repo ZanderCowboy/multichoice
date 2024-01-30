1e7a44fd63e5f9ad4a6782b849730f2233311a4a
ZanderCowboy <zanderkotze99@gmail.com>
2024-01-30 23:52:01 +0000

    fix spelling mistake

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -23,7 +23,7 @@ git config --global core.sshCommand "ssh -i /home/developer/.ssh/personal_github
 # Change HTTPS to SSH
 echo "git remote -v"
 git remote -v
-ehco "git remote set-url origin 'git@github.com:<your-username>/<repository>.git'"
+echo "git remote set-url origin 'git@github.com:<your-username>/<repository>.git'"
 git remote set-url origin git@github.com:ZanderCowboy/multichoice.git
 
 echo "git config --global --list"
