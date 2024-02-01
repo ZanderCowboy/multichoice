751d338e90b683e2ac5d69e2a27838702b391514
ZanderCowboy <zanderkotze99@gmail.com>
2024-02-01 09:25:52 +0000

    cleaned up code

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 dart --disable-analytics
-
-# Output a message indicating that telemetry has been disabled
 echo "Telemetry for Dart and Flutter has been disabled."
 
-# Run Flutter Pub Get
-# flutter upgrade
-echo "flutter pub get"
-flutter pub get
-echo "flutter pub outdated"
-flutter pub outdated
 
-echo "git config --global --list"
-git config --global --list
+flutter pub get
+echo "flutter pub get"
+flutter pub outdated
+echo "flutter pub outdated"
 
 # Change .gitconfig
 git config --global user.name "ZanderCowboy"
@@ -21,12 +15,12 @@ git config --global user.email "zanderkotze99@gmail.com"
 git config --global core.sshCommand "ssh -i /home/developer/.ssh/personal_github"
 
 # Change HTTPS to SSH
-echo "git remote -v"
 git remote -v
-echo "git remote set-url origin 'git@github.com:<your-username>/<repository>.git'"
+echo "git remote -v"
 git remote set-url origin git@github.com:ZanderCowboy/multichoice.git
+echo "git remote set-url origin 'git@github.com:ZanderCowboy/multichoice.git'"
 
-echo "git config --global --list"
 git config --global --list
+echo "git config --global --list"
 
 git config --global --add safe.directory /workspaces/multichoice
