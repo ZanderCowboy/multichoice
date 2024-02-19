9ebc2d5216f6f905bcc5b88991337af7e60d1521
Zander Kotze <Zander.Kotze@rank.com>
2024-02-19 15:08:41 +0200

    moved app.dart to view folder

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:multichoice/app.dart';
+import 'package:multichoice/app/view/app.dart';
 import 'package:multichoice/bloc_observer.dart';
 import 'package:multichoice/get_it_injection.dart';
 import 'package:window_size/window_size.dart';
