2cbac7cb83851aa004b5c0ef7b554534c36ce595
ZanderCowboy <zanderkotze99@gmail.com>
2024-03-01 02:45:41 +0200

    moved MenuAnchor to menu_widget for cleaner code

---
+++ lib/presentation/home/widgets/menu_widget.dart
@@ -0,0 +1,128 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:multichoice/app_router.gr.dart';
+import 'package:multichoice/application/home/home_bloc.dart';
+import 'package:multichoice/constants/spacing_constants.dart';
+import 'package:multichoice/models/enums/menu_items.dart';
+import 'package:multichoice/presentation/home/widgets/alert_dialog.dart';
+
+class MenuWidget extends StatelessWidget {
+  const MenuWidget({
+    required this.homeBloc,
+    required this.id,
+    super.key,
+  });
+
+  final HomeBloc homeBloc;
+  final int id;
+
+  @override
+  Widget build(BuildContext context) {
+    return MenuAnchor(
+      consumeOutsideTap: true,
+      builder: (
+        BuildContext context,
+        MenuController controller,
+        Widget? child,
+      ) {
+        return IconButton(
+          onPressed: () {
+            if (controller.isOpen) {
+              controller.close();
+            } else {
+              controller.open();
+            }
+          },
+          icon: const Icon(
+            Icons.more_vert_outlined,
+          ),
+          hoverColor: Colors.pink,
+          padding: EdgeInsets.zero,
+        );
+      },
+      menuChildren: [
+        MenuItemButton(
+          onPressed: () => {
+            context.router.push(EditPageRoute(ctx: context)),
+          },
+          child: Text(MenuItems.edit.name),
+        ),
+        MenuItemButton(
+          onPressed: () {
+            showDialog<AlertDialog>(
+              context: context,
+              builder: (BuildContext context) {
+                return BlocProvider<HomeBloc>.value(
+                  value: homeBloc,
+                  child: BlocBuilder<HomeBloc, HomeState>(
+                    builder: (context, state) {
+                      return AlertDialog(
+                        title: Text(
+                          'Delete All Entries of ${state.tab.title}?',
+                        ),
+                        content: SizedBox(
+                          height: 50,
+                          child: Text(
+                            'Are you sure you want to delete all the entries of ${state.tab.title}?',
+                          ),
+                        ),
+                        actions: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              OutlinedButton(
+                                onPressed: () => Navigator.of(
+                                  context,
+                                ).pop(),
+                                child: const Text(
+                                  'Cancel',
+                                ),
+                              ),
+                              gap10,
+                              ElevatedButton(
+                                onPressed: () {
+                                  context.read<HomeBloc>().add(
+                                        HomeEvent.onPressedDeleteEntries(
+                                          id,
+                                        ),
+                                      );
+
+                                  if (Navigator.canPop(
+                                    context,
+                                  )) {
+                                    Navigator.of(
+                                      context,
+                                    ).pop();
+                                  }
+                                },
+                                child: const Text(
+                                  'Delete Entries',
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                );
+              },
+            );
+          },
+          child: Text(MenuItems.deleteEntries.name),
+        ),
+        MenuItemButton(
+          onPressed: () {
+            show<AlertDialog>(
+              context,
+              homeBloc,
+              id,
+            );
+          },
+          child: Text(MenuItems.delete.name),
+        ),
+      ],
+    );
+  }
+}
