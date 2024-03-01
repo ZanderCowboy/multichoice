340fd7bcf7873177c132b1ad09cd91f084f28308
ZanderCowboy <zanderkotze99@gmail.com>
2024-03-01 02:47:14 +0200

    added menuAnchor to entry_card

--- lib/presentation/home/widgets/entry_card.dart
+++ lib/presentation/home/widgets/entry_card.dart
@@ -1,7 +1,7 @@
-part of '../home_page.dart';
+part of 'entry_cards.dart';
 
-class EntryCard extends StatelessWidget {
-  const EntryCard({
+class EntryCard extends HookWidget {
+  EntryCard({
     required this.title,
     required this.subtitle,
     this.tabId,
@@ -14,45 +14,121 @@ class EntryCard extends StatelessWidget {
   final String? title;
   final String? subtitle;
 
+  final _controller = MenuController();
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 7,
-      shadowColor: Colors.white,
-      shape: RoundedRectangleBorder(
-        borderRadius: circularBorder5,
-      ),
-      color: const Color.fromARGB(255, 81, 153, 187),
-      child: SizedBox(
-        width: double.infinity,
-        child: Padding(
-          padding: allPadding4,
-          child: Row(
-            children: [
-              Expanded(
-                child: Column(
+    return MenuAnchor(
+      controller: _controller,
+      consumeOutsideTap: true,
+      builder: (context, controller, child) {
+        return child ?? const SizedBox.shrink();
+      },
+      menuChildren: [
+        MenuItemButton(
+          onPressed: () {
+            context
+                .read<HomeBloc>()
+                .add(HomeEvent.onUpdateEntry(tabId!, entryId!));
+            context.router.push(EditEntryPageRoute(ctx: context));
+          },
+          child: Text(MenuItems.edit.name),
+        ),
+        MenuItemButton(
+          onPressed: () {
+            CustomDialog<Widget>.show(
+              context: context,
+              title: Text('Delete $title'),
+              content: SizedBox(
+                height: 20,
+                child: Text(
+                  "Are you sure you want to delete entry $title and all it's content?",
+                ),
+              ),
+              actions: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Text(
-                      title ?? '',
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
-                      ),
+                    OutlinedButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Text('Cancel'),
                     ),
-                    Text(
-                      subtitle ?? '',
-                      style: const TextStyle(
-                        fontSize: 14,
-                      ),
+                    gap10,
+                    ElevatedButton(
+                      onPressed: () {
+                        context.read<HomeBloc>().add(
+                              HomeEvent.onLongPressedDeleteEntry(
+                                tabId!,
+                                entryId!,
+                              ),
+                            );
+                        if (Navigator.canPop(context)) {
+                          Navigator.of(context).pop();
+                        }
+                      },
+                      child: const Text('Delete'),
                     ),
                   ],
                 ),
+              ],
+            );
+          },
+          child: Text(MenuItems.delete.name),
+        ),
+      ],
+      child: GestureDetector(
+        onDoubleTap: () {
+          context
+              .read<HomeBloc>()
+              .add(HomeEvent.onUpdateEntry(tabId!, entryId!));
+          context.router.push(EditEntryPageRoute(ctx: context));
+        },
+        onLongPress: () {
+          if (_controller.isOpen) {
+            _controller.close();
+          } else {
+            _controller.open();
+          }
+        },
+        child: Card(
+          elevation: 7,
+          shadowColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: circularBorder5,
+          ),
+          color: const Color.fromARGB(255, 81, 153, 187),
+          child: SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding: allPadding4,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          title ?? '',
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          subtitle ?? '',
+                          style: const TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Placeholder(
+                    fallbackHeight: 40,
+                    fallbackWidth: 40,
+                  ),
+                ],
               ),
-              const Placeholder(
-                fallbackHeight: 40,
-                fallbackWidth: 40,
-              ),
-            ],
+            ),
           ),
         ),
       ),
