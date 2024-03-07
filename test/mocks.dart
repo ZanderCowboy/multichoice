ddbbb4419d79bd778dcdd584ee8ed34426d11d7f
Zander Kotze <Zander.Kotze@rank.com>
2024-03-07 23:06:46 +0200

    removed dead code'

--- test/mocks.dart
+++
@@ -1,8 +0,0 @@
-import 'package:mockito/annotations.dart';
-import 'package:multichoice/repositories/export_repositories.dart';
-
-@GenerateNiceMocks([
-  MockSpec<TabsRepository>(as: #MockTabsRepository),
-  MockSpec<EntryRepository>(as: #MockEntryRepository),
-])
-void main() {}
