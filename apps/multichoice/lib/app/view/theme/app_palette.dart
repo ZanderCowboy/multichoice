0e050a2ee4d54373a619644612fa2211a0d31f0f
ZanderCowboy <zanderkotze99@gmail.com>
2024-03-18 20:35:43 +0200

    added app theming files

---
+++ apps/multichoice/lib/app/view/theme/app_palette.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+abstract class AppPalette {
+  static const white = Color(0xffffffff);
+  static const black = Color(0xff000000);
+
+  static const red = Colors.red;
+  static const imperialRed = Color(0xFFE54B4B);
+
+  static const seashell = Color(0xFFF7EBE8);
+
+  static final green = _GreenColors();
+  static final grey = _GreyColors();
+  static final paletteOne = _PaletteOne();
+  static final paletteTwo = _PaletteTwo();
+}
+
+class _GreenColors {
+  _GreenColors();
+
+  final greenPea = const Color(0xff26734d);
+  final chateauGreen = const Color(0xff37a06b);
+  final silverTree = const Color(0xff4eb18b);
+  final neptune = const Color(0xff81c1aa);
+  final jetStream = const Color(0xffa8d1c8);
+}
+
+class _GreyColors {
+  _GreyColors();
+
+  final bigStone = const Color(0xff15203c);
+  final sanJuan = const Color(0xff385170);
+  final sanJuanLight = const Color(0x7f385170);
+  final slateGray = const Color(0xff697a91);
+  final towerGray = const Color(0xffa1baba);
+  final geyser = const Color(0xffd7e0e5);
+  final geyserLight = const Color(0x7fd7e0e5);
+}
+
+class _PaletteTwo {
+  _PaletteTwo();
+
+  final bigStone = const Color(0xff171d2c);
+  final sanJuan = const Color(0xffe7ecef);
+  final sanJuanLight = const Color(0x7f172435);
+  final slateGray = const Color(0xff555d68);
+  final slateGrayLight = const Color(0x7f555d68);
+  final towerGray = const Color(0xff4e5c5c);
+  final geyser = const Color(0xff172435);
+  final geyserLight = const Color(0x7f172435);
+}
+
+class _PaletteOne {
+  final darkSpringGreen = const Color(0xff226f54);
+  final pistachio = const Color(0xff87c38f);
+  final lemonChiffon = const Color(0xfff4f0bb);
+  final black = const Color(0xff000000);
+  final quinacridoneMagenta = const Color(0xff8f2d56);
+}
---
+++ apps/multichoice/lib/app/view/theme/app_theme.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:multichoice/app/view/theme/app_palette.dart';
+import 'package:multichoice/app/view/theme/app_typography.dart';
+import 'package:multichoice/constants/export_constants.dart';
+import 'package:theme/theme.dart';
+
+class AppTheme with ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  ThemeMode get themeMode => _themeMode;
+
+  set themeMode(ThemeMode themeMode) {
+    _themeMode = themeMode;
+    notifyListeners();
+  }
+
+  static final light = () {
+    final defaultTheme = ThemeData.light();
+
+    return defaultTheme.copyWith(
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: OutlinedButton.styleFrom(
+          foregroundColor: _lightAppColors.primary,
+          side: BorderSide(color: _lightAppColors.primary ?? Colors.white),
+          shape: RoundedRectangleBorder(borderRadius: borderCircular12),
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: _lightAppColors.secondary,
+          shape: RoundedRectangleBorder(borderRadius: borderCircular12),
+        ),
+      ),
+      dialogBackgroundColor: _lightAppColors.background,
+      dialogTheme: DialogTheme(
+        shape: RoundedRectangleBorder(borderRadius: borderCircular16),
+        alignment: Alignment.center,
+        titleTextStyle: AppTypography.titleMedium,
+        contentTextStyle: AppTypography.bodyMedium,
+        actionsPadding: allPadding12,
+      ),
+      scaffoldBackgroundColor: _lightAppColors.background,
+      appBarTheme: AppBarTheme(
+        titleTextStyle: AppTypography.titleMedium.copyWith(
+          color: AppPalette.grey.geyser,
+        ),
+        centerTitle: true,
+        color: _lightAppColors.foreground,
+      ),
+      cardTheme: CardTheme(
+        margin: vertical12horizontal4,
+        elevation: 7,
+        shadowColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: borderCircular12),
+      ),
+      textTheme: defaultTheme.textTheme.copyWith(
+        titleMedium: _lightTextTheme.titleMedium,
+        bodyMedium: _lightTextTheme.bodyMedium,
+      ),
+      iconButtonTheme: IconButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStatePropertyAll(AppPalette.grey.geyser),
+          padding: const MaterialStatePropertyAll(EdgeInsets.zero),
+          side: const MaterialStatePropertyAll(BorderSide.none),
+        ),
+      ),
+      iconTheme: IconThemeData(
+        size: 18,
+        color: _lightAppColors.primary,
+      ),
+      extensions: [
+        _lightAppColors,
+        _lightTextTheme,
+      ],
+    );
+  }();
+
+  static final _lightAppColors = AppColorsExtension(
+    primary: AppPalette.grey.geyser,
+    primaryLight: AppPalette.grey.geyserLight,
+    secondary: AppPalette.grey.sanJuan,
+    secondaryLight: AppPalette.grey.sanJuanLight,
+    ternary: null,
+    foreground: AppPalette.grey.bigStone,
+    background: AppPalette.grey.slateGray,
+    white: null,
+    black: null,
+  );
+
+  static final _lightTextTheme = AppTextExtension(
+    body1: AppTypography.body1.copyWith(color: _lightAppColors.background),
+    body2: AppTypography.body2,
+    h1: null,
+    titleLarge: null,
+    titleMedium: AppTypography.titleMedium.copyWith(
+      color: AppPalette.grey.bigStone,
+    ),
+    titleSmall: AppTypography.titleSmall.copyWith(
+      color: AppPalette.grey.bigStone,
+    ),
+    subtitleLarge: null,
+    subtitleMedium: null,
+    subtitleSmall: null,
+    bodyLarge: null,
+    bodyMedium: AppTypography.bodyMedium.copyWith(
+      color: AppPalette.grey.geyser,
+    ),
+    bodySmall: null,
+  );
+
+  static final dark = () {
+    final defaultTheme = ThemeData.dark();
+
+    return defaultTheme.copyWith(
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: OutlinedButton.styleFrom(
+          foregroundColor: _darkAppColors.foreground,
+          textStyle: AppTypography.bodyLarge,
+          side: BorderSide(color: _darkAppColors.primary ?? Colors.white),
+          shape: RoundedRectangleBorder(borderRadius: borderCircular12),
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: AppPalette.paletteTwo.slateGray,
+          backgroundColor: AppPalette.paletteTwo.geyser,
+          shape: RoundedRectangleBorder(borderRadius: borderCircular12),
+          textStyle: AppTypography.bodyLarge,
+        ),
+      ),
+      dialogBackgroundColor: _darkAppColors.background,
+      dialogTheme: DialogTheme(
+        surfaceTintColor: Colors.transparent,
+        shape: RoundedRectangleBorder(borderRadius: borderCircular16),
+        alignment: Alignment.center,
+        titleTextStyle: AppTypography.titleMedium,
+        contentTextStyle: AppTypography.bodyMedium,
+        actionsPadding: allPadding12,
+      ),
+      scaffoldBackgroundColor: _darkAppColors.background,
+      appBarTheme: AppBarTheme(
+        titleTextStyle: AppTypography.titleMedium.copyWith(
+          color: AppPalette.paletteTwo.sanJuan,
+        ),
+        centerTitle: true,
+        color: _darkAppColors.foreground,
+      ),
+      cardTheme: CardTheme(
+        margin: vertical12horizontal4,
+        elevation: 7,
+        shadowColor: _darkAppColors.black,
+        shape: RoundedRectangleBorder(borderRadius: borderCircular12),
+      ),
+      textTheme: defaultTheme.textTheme.copyWith(
+        titleMedium: _darkTextTheme.titleMedium,
+        bodyMedium: _darkTextTheme.bodyMedium,
+      ),
+      iconButtonTheme: IconButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor:
+              MaterialStatePropertyAll(AppPalette.paletteTwo.sanJuan),
+          padding: const MaterialStatePropertyAll(EdgeInsets.zero),
+          side: const MaterialStatePropertyAll(BorderSide.none),
+        ),
+      ),
+      iconTheme: IconThemeData(
+        size: 18,
+        color: AppPalette.paletteTwo.sanJuan,
+      ),
+      extensions: [
+        _darkAppColors,
+        _darkTextTheme,
+      ],
+    );
+  }();
+
+  static final _darkAppColors = AppColorsExtension(
+    primary: AppPalette.paletteTwo.geyser,
+    primaryLight: AppPalette.paletteTwo.geyserLight,
+    secondary: AppPalette.paletteTwo.slateGray,
+    secondaryLight: AppPalette.paletteTwo.slateGrayLight,
+    ternary: null,
+    foreground: AppPalette.paletteTwo.bigStone,
+    background: AppPalette.paletteTwo.slateGray,
+    white: null,
+    black: AppPalette.black,
+  );
+
+  static final _darkTextTheme = AppTextExtension(
+    body1: AppTypography.body1.copyWith(color: _darkAppColors.background),
+    body2: AppTypography.body2,
+    h1: null,
+    titleLarge: null,
+    titleMedium: AppTypography.titleMedium.copyWith(
+      color: AppPalette.paletteTwo.sanJuan,
+    ),
+    titleSmall: AppTypography.titleSmall.copyWith(
+      color: AppPalette.paletteTwo.sanJuan,
+    ),
+    subtitleLarge: null,
+    subtitleMedium: null,
+    subtitleSmall: null,
+    bodyLarge: AppTypography.bodyLarge.copyWith(
+      color: AppPalette.paletteTwo.sanJuan,
+    ),
+    bodyMedium: AppTypography.bodyMedium.copyWith(
+      color: AppPalette.paletteTwo.sanJuan,
+    ),
+    bodySmall: null,
+  );
+
+  static AppColorsExtension get lightAppColors => _lightAppColors;
+
+  static AppTextExtension get lightTextTheme => _lightTextTheme;
+
+  static AppColorsExtension get darkAppColors => _darkAppColors;
+
+  static AppTextExtension get darkTextTheme => _darkTextTheme;
+}
