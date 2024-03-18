7c49407b391a41e937c20e6b0fa33ff00d55d8ce
ZanderCowboy <zanderkotze99@gmail.com>
2024-03-18 20:36:41 +0200

    added onPressedTheme event to HomeBloc

--- packages/core/lib/src/application/home/home_event.dart
+++ packages/core/lib/src/application/home/home_event.dart
@@ -35,4 +35,6 @@ class HomeEvent with _$HomeEvent {
       OnPressedDeleteAllEntries;
 
   const factory HomeEvent.onPressedDeleteAll() = OnPressedDeleteAll;
+
+  const factory HomeEvent.onPressedTheme() = OnPressedTheme;
 }
