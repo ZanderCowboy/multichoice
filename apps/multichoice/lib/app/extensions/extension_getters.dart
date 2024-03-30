2f97b2ab8dac0bb0dda7dec03c02028807a12f04
ZanderCowboy <zanderkotze99@gmail.com>
2024-03-30 20:32:40 +0200

    renamed theme_getter to extension_getters

--- apps/multichoice/lib/app/extensions/extension_getters.dart
+++ apps/multichoice/lib/app/extensions/extension_getters.dart
@@ -4,3 +4,7 @@ extension ThemeGetter on BuildContext {
   // Usage example: `context.theme`
   ThemeData get theme => Theme.of(this);
 }
+
+// extension SizeGetter on BuildContext {
+//   Size get size => MediaQuery.of(this).size;
+// }
