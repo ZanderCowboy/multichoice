b62532f0c8f1d4e2c25f22550f1074778e23f893
ZanderCowboy <zanderkotze99@gmail.com>
2024-04-01 01:42:29 +0200

    cleaned up code for persisting app theme

--- apps/multichoice/lib/app/view/app.dart
+++ apps/multichoice/lib/app/view/app.dart
@@ -1,9 +1,7 @@
-import 'package:core/core.dart';
 import 'package:flutter/material.dart';
 import 'package:multichoice/app/engine/app_router.dart';
 import 'package:multichoice/app/view/theme/app_theme.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class App extends StatelessWidget {
   App({super.key});
@@ -12,14 +10,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final sharedPref = coreSl<SharedPreferences>();
-    final systemBrightness = MediaQuery.platformBrightnessOf(context);
-    final isDarkMode = systemBrightness == Brightness.dark;
-
-    if (sharedPref.getString('theme') == 'light' && isDarkMode) {
-      sharedPref.setString('theme', 'dark');
-    }
-
     return ChangeNotifierProvider(
       create: (context) => AppTheme(),
       builder: (context, child) => MaterialApp.router(
