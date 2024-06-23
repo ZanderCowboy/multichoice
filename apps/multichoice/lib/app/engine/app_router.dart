72ae9f46dcb378528956a7ebc83b3e258a3dd826
ZanderCowboy <zanderkotze99@gmail.com>
2024-06-23 16:35:09 +0200

    create and add 'DataTransferPage'

--- apps/multichoice/lib/app/engine/app_router.dart
+++ apps/multichoice/lib/app/engine/app_router.dart
@@ -5,6 +5,7 @@ import 'package:multichoice/app/engine/app_router.gr.dart';
 class AppRouter extends $AppRouter {
   @override
   List<AutoRoute> get routes => [
+        AutoRoute(page: DataTransferScreenRoute.page),
         AutoRoute(page: HomePageRoute.page, initial: true),
         AutoRoute(page: EditTabPageRoute.page),
         AutoRoute(page: EditEntryPageRoute.page),
