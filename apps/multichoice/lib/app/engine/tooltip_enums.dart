9797d95a29850a4b8ce26a3747c5a4caf476ddf5
ZanderCowboy <zanderkotze99@gmail.com>
2024-06-23 16:36:24 +0200

    add TooltipEnums file

---
+++ apps/multichoice/lib/app/engine/tooltip_enums.dart
@@ -0,0 +1,25 @@
+enum TooltipEnums {
+  back('Back'),
+  newTab('New Tab'),
+  newEntry('New Entry'),
+  search('Search'),
+  tabOptions('Tab Options'),
+  ok('Ok'),
+  cancel('Cancel'),
+  close('Close'),
+  lightDarkMode('Light/Dark Mode'),
+  deleteAllData('Delete All Data'),
+  deleteTab('Delete Tab'),
+  deleteEntry('Delete Entry'),
+  editTab('Edit Tab'),
+  editEntry('Edit Entry'),
+  importExport('Import/Export'),
+  import('Import'),
+  export('Export'),
+  settings('Settings'),
+  home('Home');
+
+  const TooltipEnums(this.tooltip);
+
+  final String tooltip;
+}
