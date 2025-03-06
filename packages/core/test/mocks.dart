8de54af88329b8fe49a055bdbba999813c30b82a
ZanderCowboy <zanderkotze99@gmail.com>
2025-03-06 18:06:38 +0200

    write test for data_exchange_service

--- packages/core/test/mocks.dart
+++ packages/core/test/mocks.dart
@@ -1,8 +1,12 @@
 import 'package:core/src/repositories/export_repositories.dart';
+import 'package:core/src/wrappers/export.dart';
+import 'package:isar/isar.dart';
 import 'package:mockito/annotations.dart';
 
 @GenerateNiceMocks([
   MockSpec<TabsRepository>(as: #MockTabsRepository),
   MockSpec<EntryRepository>(as: #MockEntryRepository),
+  MockSpec<Isar>(as: #MockIsar),
+  MockSpec<IFilePickerWrapper>(as: #MockFilePickerWrapper)
 ])
 void main() {}
