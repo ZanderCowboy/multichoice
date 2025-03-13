ed9facd11a843fe45fdf9e7fa42a6065903f7dba
ZanderCowboy <zanderkotze99@gmail.com>
2025-03-13 12:35:47 +0200

    import changes to use export files

--- apps/multichoice/lib/layouts/home_layout/home_layout.dart
+++ apps/multichoice/lib/layouts/home_layout/home_layout.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:models/models.dart';
 import 'package:multichoice/app/view/layout/app_layout.dart';
-import 'package:multichoice/constants/export_constants.dart';
+import 'package:multichoice/constants/export.dart';
 import 'package:multichoice/presentation/home/home_page.dart';
 import 'package:multichoice/utils/custom_scroll_behaviour.dart';
 import 'package:provider/provider.dart';
