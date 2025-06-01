d9f1539f72bdece72e0cd7733bb83d73b918d2b6
ZanderCowboy <zanderkotze99@gmail.com>
2025-06-01 18:04:49 +0200

    chore: merge changes

--- packages/core/lib/src/services/export.dart
+++ packages/core/lib/src/services/export.dart
@@ -1,2 +1,4 @@
 export 'interfaces/i_app_info_service.dart';
 export 'interfaces/i_data_exchange_service.dart';
+export 'interfaces/i_progress_service.dart';
+export 'implementations/progress_service.dart';
