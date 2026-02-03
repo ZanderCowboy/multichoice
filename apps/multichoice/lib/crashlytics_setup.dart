2f85d9d627ec94e8e77296db56b9353cdb0abfc1
Zander Kotze <zanderkotze99@gmail.com>
2026-02-04 00:20:25 +0200

    feat: add code setupCrashlytics to main

---
+++ apps/multichoice/lib/crashlytics_setup.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: discarded_futures, document_ignores
+
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+
+/// Sets up Firebase Crashlytics error handlers to catch and report
+/// all uncaught errors in the Flutter app.
+///
+/// This function should be called after Firebase is initialized
+/// and before runApp is called.
+void setupCrashlytics() {
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = (errorDetails) async {
+    await FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  // Pass all uncaught asynchronous errors that aren't handled by the
+  // Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+}
--- apps/multichoice/lib/main.dart
+++ apps/multichoice/lib/main.dart
@@ -5,10 +5,13 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:multichoice/app/export.dart';
 import 'package:multichoice/bootstrap.dart';
+import 'package:multichoice/crashlytics_setup.dart';
 import 'package:window_size/window_size.dart';
 
 void main() async {
   await bootstrap();
+
+  setupCrashlytics();
 
   try {
     if (!kIsWeb) {
