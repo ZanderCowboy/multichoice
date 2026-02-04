d7f7bffaa29289a6580250f833b482d337f57857
Zander Kotze <zanderkotze99@gmail.com>
2026-02-04 21:56:07 +0200

    chore: linting and package updates

--- packages/core/lib/src/application/firebase/firebase_state.dart
+++ packages/core/lib/src/application/firebase/firebase_state.dart
@@ -1,12 +1,12 @@
 part of 'firebase_bloc.dart';
 
 @freezed
-class FirebaseState with _$FirebaseState {
+abstract class FirebaseState with _$FirebaseState {
   const factory FirebaseState({
     required String color,
   }) = _FirebaseState;
 
   factory FirebaseState.initial() => FirebaseState(
-        color: '',
-      );
+    color: '',
+  );
 }
