986e036e5f536ecccd5458cf33238ef2b4db388f
Zander Kotze <zanderkotze99@gmail.com>
2026-02-06 01:34:46 +0200

    chore: .cursor files - examples only

---
+++ .cursor/templates/example-test-template.dart
@@ -0,0 +1,28 @@
+// Template for creating a new test file
+// Copy this file and replace <ClassName> with the class you're testing
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('<ClassName>', () {
+    setUp(() {
+      // Setup code here
+    });
+
+    tearDown(() {
+      // Cleanup code here
+    });
+
+    test('should <expected behavior> when <condition>', () async {
+      // Arrange
+      // Act
+      // Assert
+    });
+
+    test('should handle <error case>', () async {
+      // Arrange
+      // Act
+      // Assert
+    });
+  });
+}
