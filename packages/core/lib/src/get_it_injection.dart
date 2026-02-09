5b8f4fc2108c2ec9dd2c833dbb53bed9a64f4e3b
Zander Kotze <zanderkotze99@gmail.com>
2026-02-09 13:57:09 +0200

    chore: fix dependency injection issue

--- packages/core/lib/src/get_it_injection.dart
+++ packages/core/lib/src/get_it_injection.dart
@@ -1,4 +1,5 @@
 import 'package:core/src/get_it_injection.config.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
@@ -10,5 +11,14 @@ final coreSl = GetIt.instance;
   asExtension: true,
 )
 Future<GetIt> configureCoreDependencies() async {
+  // Manually register FirebaseRemoteConfig so that it can be injected into
+  // FirebaseService via GetIt. This avoids modifying generated files while
+  // ensuring the dependency is available during initialization.
+  if (!coreSl.isRegistered<FirebaseRemoteConfig>()) {
+    coreSl.registerLazySingleton<FirebaseRemoteConfig>(
+      () => FirebaseRemoteConfig.instance,
+    );
+  }
+
   return coreSl.init();
 }
