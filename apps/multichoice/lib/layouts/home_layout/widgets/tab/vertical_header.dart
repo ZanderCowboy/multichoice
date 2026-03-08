5dd4b100a7908e407eb9315eaab09baa24041d87
Zander Kotze <zanderkotze99@gmail.com>
2026-03-08 15:54:25 +0200

    refactor: move vertical_header to a separate widget

---
+++ apps/multichoice/lib/layouts/home_layout/widgets/tab/vertical_header.dart
@@ -0,0 +1,67 @@
+part of '../../tab_layout.dart';
+
+class VerticalHeader extends StatelessWidget {
+  const VerticalHeader({
+    required this.dragIndex,
+    required this.isEditMode,
+    required this.tab,
+    super.key,
+  });
+
+  final bool isEditMode;
+  final int? dragIndex;
+  final TabsDTO tab;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: allPadding4,
+        child: Row(
+          children: [
+            if (isEditMode && dragIndex != null)
+              Center(
+                child: ReorderableDragStartListener(
+                  index: dragIndex!,
+                  child: Icon(
+                    Icons.drag_handle,
+                    size: 28,
+                    color: context.theme.appColors.ternary,
+                  ),
+                ),
+              )
+            else if (isEditMode)
+              Center(
+                child: Icon(
+                  Icons.drag_handle,
+                  size: 28,
+                  color: context.theme.appColors.ternary,
+                ),
+              ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    tab.title,
+                    style: context.theme.appTextTheme.titleMedium,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 3,
+                  ),
+                  if (tab.subtitle.isNotEmpty)
+                    Text(
+                      tab.subtitle,
+                      style: context.theme.appTextTheme.subtitleMedium,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 3,
+                    ),
+                ],
+              ),
+            ),
+            if (!isEditMode) MenuWidget(tab: tab),
+          ],
+        ),
+      ),
+    );
+  }
+}
