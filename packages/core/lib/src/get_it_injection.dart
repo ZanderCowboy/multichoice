d2a949381db9dc6f0542b7b8c1850816e34f9bb1
Zander Kotze <zanderkotze99@gmail.com>
2026-03-09 00:54:58 +0200

    chore: update get_it_injection

--- packages/core/lib/src/get_it_injection.dart
+++ packages/core/lib/src/get_it_injection.dart
@@ -1,6 +1,4 @@
 import 'package:core/src/get_it_injection.config.dart';
-import 'package:core/src/services/implementations/analytics_service.dart';
-import 'package:core/src/services/interfaces/i_analytics_service.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:get_it/get_it.dart';
@@ -17,12 +15,6 @@ Future<GetIt> configureCoreDependencies() async {
   if (!coreSl.isRegistered<FirebaseAnalytics>()) {
     coreSl.registerLazySingleton<FirebaseAnalytics>(
       () => FirebaseAnalytics.instance,
-    );
-  }
-
-  if (!coreSl.isRegistered<IAnalyticsService>()) {
-    coreSl.registerLazySingleton<IAnalyticsService>(
-      () => FirebaseAnalyticsService(),
     );
   }
 
