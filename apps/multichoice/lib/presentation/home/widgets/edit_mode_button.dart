86a03bcd2bcbda1d695c65fb1a940f65af1fecf7
Zander Kotze <zanderkotze99@gmail.com>
2026-03-08 12:43:35 +0200

    refactor: create search_button.dart, edit_mode_button.dart, _trigger_edit_mode_haptic.dart and change HomePage to StatefulWidget

---
+++ apps/multichoice/lib/presentation/home/widgets/edit_mode_button.dart
@@ -0,0 +1,28 @@
+part of '../home_page.dart';
+
+class EditModeButton extends StatelessWidget {
+  const EditModeButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<HomeBloc, HomeState>(
+      builder: (context, state) {
+        return IconButton(
+          onPressed: () async {
+            if (!state.isEditMode) {
+              await _triggerEditModeHaptic();
+            }
+            // ignore: use_build_context_synchronously
+            context.read<HomeBloc>().add(
+              const HomeEvent.onToggleEditMode(),
+            );
+          },
+          tooltip: state.isEditMode ? 'Finish editing' : 'Edit order',
+          icon: Icon(
+            state.isEditMode ? Icons.check : Icons.edit_outlined,
+          ),
+        );
+      },
+    );
+  }
+}
