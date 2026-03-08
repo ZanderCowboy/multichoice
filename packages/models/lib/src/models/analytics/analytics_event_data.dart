cfe2ab13b8ea435aea0577fc67dbe9f8848e7bf3
Zander Kotze <zanderkotze99@gmail.com>
2026-03-08 21:05:09 +0200

    chore: create data classes for Analytics

---
+++ packages/models/lib/src/models/analytics/analytics_event_data.dart
@@ -0,0 +1,32 @@
+import 'package:models/src/enums/analytics/export.dart';
+
+abstract class AnalyticsEventData {
+  const AnalyticsEventData();
+
+  AnalyticsEventName get eventName;
+
+  Map<AnalyticsParamKey, Object?> get parameters;
+
+  Map<String, Object> toFirebaseParameters() {
+    final params = <String, Object>{};
+
+    for (final entry in parameters.entries) {
+      final value = entry.value;
+      if (value == null) {
+        continue;
+      }
+
+      params[entry.key.key] = _normalizeValue(value);
+    }
+
+    return params;
+  }
+
+  Object _normalizeValue(Object value) {
+    if (value is String || value is num || value is bool) {
+      return value;
+    }
+
+    return value.toString();
+  }
+}
---
+++ packages/models/lib/src/models/analytics/export.dart
@@ -0,0 +1,8 @@
+export 'analytics_event_data.dart';
+export 'crud_event_data.dart';
+export 'feedback_event_data.dart';
+export 'screen_view_event_data.dart';
+export 'search_event_data.dart';
+export 'search_result_opened_event_data.dart';
+export 'tutorial_event_data.dart';
+export 'ui_action_event_data.dart';
