89b40e7587c35ce42b5708a516ccacced52c7047
Zander Kotze <zanderkotze99@gmail.com>
2026-03-09 00:43:25 +0200

    refactor: bootstrap

---
+++ apps/multichoice/lib/app/bootstrap/bootstrap.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:core/core.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:multichoice/firebase_options.dart';
+
+Future<void> bootstrap() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await setupFirebase();
+
+  await configureCoreDependencies();
+}
+
+Future<void> setupFirebase() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
--- apps/multichoice/lib/app/bootstrap/non_critical_services.dart
+++ apps/multichoice/lib/app/bootstrap/non_critical_services.dart
@@ -3,54 +3,10 @@
 import 'dart:async';
 import 'dart:developer';
 
-import 'package:bloc/bloc.dart';
 import 'package:core/core.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:models/models.dart';
-import 'package:multichoice/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-class SimpleBlocObserver extends BlocObserver {
-  const SimpleBlocObserver();
-
-  @override
-  void onEvent(
-    Bloc<dynamic, dynamic> bloc,
-    Object? event,
-  ) {
-    super.onEvent(bloc, event);
-    log('${bloc.runtimeType} $event');
-  }
-
-  @override
-  void onError(
-    BlocBase<dynamic> bloc,
-    Object error,
-    StackTrace stackTrace,
-  ) {
-    log('${bloc.runtimeType} $error');
-    super.onError(bloc, error, stackTrace);
-  }
-
-  @override
-  void onTransition(
-    Bloc<dynamic, dynamic> bloc,
-    Transition<dynamic, dynamic> transition,
-  ) {
-    super.onTransition(bloc, transition);
-    log('$transition');
-  }
-}
-
-Future<void> bootstrap() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await configureCoreDependencies();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-}
 
 Future<void> initializeNonCriticalServices() async {
   // Run Remote Config setup after first frame to avoid blocking startup.
