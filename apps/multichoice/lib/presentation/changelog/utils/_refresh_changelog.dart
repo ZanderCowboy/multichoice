ae94b7c55ae3371e06ef67b90eb250f61eda07fa
Zander Kotze <zanderkotze99@gmail.com>
2026-03-10 22:12:38 +0200

    chore: small changes

--- apps/multichoice/lib/presentation/changelog/utils/_refresh_changelog.dart
+++ apps/multichoice/lib/presentation/changelog/utils/_refresh_changelog.dart
@@ -2,15 +2,12 @@ part of '../changelog_page.dart';
 
 Future<void> _refreshChangelog(BuildContext context) async {
   try {
-    // Force refresh Firebase Remote Config immediately (bypasses minimumFetchInterval)
     final firebaseService = coreSl<IFirebaseService>();
     await firebaseService.forceFetchAndActivate();
 
-    // Trigger changelog refetch
     if (context.mounted) {
       context.read<ChangelogBloc>().add(const ChangelogEvent.fetch());
     }
-    // ignore: avoid_catches_without_on_clauses
   } catch (e) {
     if (context.mounted) {
       ScaffoldMessenger.of(context).showSnackBar(
